65151cd921caf8b2825c985133063bf80c4d26e4
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-08-28 02:37:15 +0530

    Update authSlice.js

--- frontend/src/reduxtoolkit/auth/authSlice.js
+++ frontend/src/reduxtoolkit/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/users/';
+const API_URL = 'https://task-tracker-api-theta.vercel.app/api/users/';
 const user = JSON.parse(localStorage.getItem('user'));
 
 export const register = createAsyncThunk(
